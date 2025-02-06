e8963ea35ea459d980a9fbdeeb88fc01e44b962a
sofieschn <sofiesch@kth.se>
2025-02-06 10:58:04 +0100

    as of now 1.6

--- ir/Engine.java
+++ ir/Engine.java
@@ -137,20 +137,6 @@ public class Engine {
     /* ----------------------------------------------- */
 
 
-    public Engine(String[] args) {
-        // ...
-        String indexFile = "hashedIndex.txt"; // Choose a file name
-        File file = new File(indexFile);
-        if (file.exists()) {
-            index.loadIndexFromFile(indexFile);
-        } else {
-            // Create a new index and save it to the file
-            index = new HashedIndex();
-            // ... (rest of the constructor remains the same)
-            index.saveIndexToFile(indexFile);
-        }
-        // ...
-    }
 
     
 
